055ae00cdbd079f6f9a16bc7f79f90c00fc6a39f
David <dave.obat@student.moringashool.com>
2023-10-16 12:37:24 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    addingEventListener()
+})
+
 function addingEventListener() {
+input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});   
 }
+
+let input;
